4401f4b1dbf1d547cf1d1b404257eeecf574296e
bbogomilov1 <bdbogomilov@gmail.com>
2023-05-22 19:14:15 +0300

    initializing project

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+
+const PORT = 5000;
+
+app.get("/", (req, res) => {
+  res.send("Hello from Express");
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}...`);
+});
