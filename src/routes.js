c4d11a5e17605deac62ab255e4847ce3f9634a91
bbogomilov1 <bdbogomilov@gmail.com>
2023-05-23 22:44:06 +0300

    rendering error page

--- src/routes.js
+++ src/routes.js
@@ -2,5 +2,8 @@ const router = require("express").Router();
 const homeController = require("./controllers/homeController");
 
 router.use(homeController);
+router.use("*", (req, res) => {
+  res.redirect("/404");
+});
 
 module.exports = router;
