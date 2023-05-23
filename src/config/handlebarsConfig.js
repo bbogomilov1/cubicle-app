12475766da914e9bdeb62d24f1a2bf7fefc55f4b
bbogomilov1 <bdbogomilov@gmail.com>
2023-05-23 22:38:04 +0300

    rendering home page

--- src/config/handlebarsConfig.js
+++ src/config/handlebarsConfig.js
@@ -1,7 +1,12 @@
 const handlebars = require("express-handlebars");
 
 function handlebarsConfig(app) {
-  app.engine("hbs", handlebars.engine({ extname: "hbs" }));
+  app.engine(
+    "hbs",
+    handlebars({
+      extname: "hbs",
+    })
+  );
   app.set("view engine", "hbs");
   app.set("views", "src/views");
 }
