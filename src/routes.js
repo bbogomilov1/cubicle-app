34de1264969271f4bb2b48d8cb276cfdbab29db6
bbogomilov1 <bdbogomilov@gmail.com>
2023-05-24 11:54:09 +0300

    adding create page

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,11 @@
 const router = require("express").Router();
 const homeController = require("./controllers/homeController");
+const cubeController = require("./controllers/cubeController");
 
 router.use(homeController);
-router.use("*", (req, res) => {
-  res.redirect("/404");
-});
+router.use(cubeController);
+// router.use("*", (req, res) => {
+//   res.redirect("/404");
+// });
 
 module.exports = router;
