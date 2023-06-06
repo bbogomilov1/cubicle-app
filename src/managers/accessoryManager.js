723f6b54665aff353b58aa7317d430a1637af03d
bbogomilov1 <bdbogomilov@gmail.com>
2023-06-06 23:59:23 +0300

    show remaining accessories only

--- src/managers/accessoryManager.js
+++ src/managers/accessoryManager.js
@@ -2,6 +2,9 @@ const Accessory = require("../models/AccessoryModel");
 
 exports.getAll = () => Accessory.find();
 
+exports.getOthers = (accessoryIds) =>
+  Accessory.find({ _id: { $nin: accessoryIds } });
+
 exports.create = (accessoryData) => {
   Accessory.create(accessoryData);
 };
