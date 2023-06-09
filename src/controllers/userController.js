543d40dbae2e13fc99a3e2a803cde2f9fad18695
bbogomilov1 <bdbogomilov@gmail.com>
2023-06-09 13:44:44 +0300

    added base user model

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -4,4 +4,10 @@ router.get("/register", (req, res) => {
   res.render("/users/register");
 });
 
+router.post("/register", (req, res) => {
+  const { username, password, repeatPassword } = req.body;
+
+  res.redirect("/users/login");
+});
+
 module.exports = router;
